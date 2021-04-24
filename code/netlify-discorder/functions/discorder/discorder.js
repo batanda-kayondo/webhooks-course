6d704847774cfe4bfd48bfa18824a9e5cabcc383
Batanda Kayondo <batanda.kayondo@gmail.com>
2021-04-24 14:24:33 +0300

    listening to bitrix24 webhook

--- code/netlify-discorder/functions/discorder/discorder.js
+++ code/netlify-discorder/functions/discorder/discorder.js
@@ -1,23 +1,31 @@
 const axios = require("axios").default;
+// const bodyParser = require('body-parser');
 
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 exports.handler = async (event, context) => {
   try {
-    const body = JSON.parse(event.body);
-    const username = body.sender.login;
-    const avatarUrl = body.sender.avatar_url;
-    const repoName = body.repository.name;
-    const res = await axios.post(process.env.DISCORD_WEBHOOK_URL, {
-      content: `:taco: :taco: :taco: ${username} just starred ${repoName}! :rocket: :muscle: :tada: :taco:`,
-      embeds: [
-        {
-          image: {
-            url: avatarUrl,
-          },
-        },
-      ],
-    });
-    console.log("Submitted!");
+    // console.log('event.body');
+    const jsonObject = JSON.parse('{"' + decodeURI(event.body).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"').replace(/\s/g, '') + '"}')
+    console.log(jsonObject);
+    console.log('==================================>');
+    console.log(jsonObject.data[FIELDS][ID]);
+    // const body = JSON.parse(event.body);
+    // console.log(event.urlencoded({ extended: false }));
+    // const username = body.sender.login;
+    // const avatarUrl = body.sender.avatar_url;
+    // const repoName = body.repository.name;
+    // const res = await axios.post(process.env.DISCORD_WEBHOOK_URL, {
+    //   content: `:taco: :taco: :taco: ${username} just starred ${repoName}! :rocket: :muscle: :tada: :taco:`,
+    //   embeds: [
+    //     {
+    //       image: {
+    //         url: avatarUrl,
+    //       },
+    //     },
+    //   ],
+    // });
+    // console.log("Submitted!");
+    // console.log(body);
     return {
       statusCode: 204,
     };
